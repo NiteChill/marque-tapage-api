aafd099ee5b37860e5d5104fbcc19bf78c953c67
NiteChill <puissantachille@gmail.com>
2025-11-25 17:59:29 +0100

    rm files

--- src/db/db-setup.ts
+++ src/db/db-setup.ts
@@ -3,11 +3,14 @@ import path from 'path';
 import db from './database';
 
 const sqlPath = path.join(__dirname, 'schema.sql');
+const dummyDataPath = path.join(__dirname, 'dummy-data.sql');
 
 try {
 	const schema = fs.readFileSync(sqlPath, 'utf-8');
+	const dummyData = fs.readFileSync(dummyDataPath, 'utf-8');
 
 	db.exec(schema);
+	db.exec(dummyData);
 
 	console.log('Database successfully initialized!');
 } catch (error) {
